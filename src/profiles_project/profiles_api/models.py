52a171a0f82cb785f10f55ff99bee6e3fc310373
Seng Wee <sengwee_new@hotmail.com>
2019-03-24 23:59:27 +0800

    setting up model.py

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
 # Create your models here.
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """
+    Represent a user profile inside our system
+    """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """
+        Used to get a user's full name
+        :return: String
+        """
+
+        return self.name
+
+    def get_short_name(self):
+        """
+        Used to get the user's short name
+        :return: String
+        """
+
+        return self.name
+
+    def __str__(self):
+        """
+        Django uses this when it needs to convert the object to a string
+        :return: String
+        """
+
+        return self.email
