f7da7836554c3bc46782ce27d116fb09256c76c7
Seng Wee <sengwee_new@hotmail.com>
2019-03-25 21:10:03 +0800

    create user profile viewset

--- src/profiles_project/profiles_api/serializers.py
+++ src/profiles_project/profiles_api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from . import models
 
 
 class HelloSerializer(serializers.Serializer):
@@ -7,3 +8,31 @@ class HelloSerializer(serializers.Serializer):
     """
 
     name = serializers.CharField(max_length=10)
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """
+    A serializer for our user profile objects
+    """
+
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        """
+        Create and return a new user
+        :param validated_data:
+        :return:
+        """
+
+        user = models.UserProfile(
+            email=validated_data['email'],
+            name=validated_data['name']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
