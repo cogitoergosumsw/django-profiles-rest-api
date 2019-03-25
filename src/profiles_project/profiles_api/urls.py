f3618fa134cf58e921296121c12a181cb3f170c4
Seng Wee <sengwee_new@hotmail.com>
2019-03-25 20:01:33 +0800

    added create, retrieve, update, partial_update, destroy to viewset

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register('hellow-viewset', views.HelloViewSet, base_name='hello-viewset')
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 
 urlpatterns = [
     url(r'^hello-view/', views.HelloApiView.as_view()),
