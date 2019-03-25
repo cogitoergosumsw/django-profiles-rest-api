8f950fabbf0cb6a25b1d85299c36bddd1a1b0e63
Seng Wee <sengwee_new@hotmail.com>
2019-03-25 17:08:05 +0800

    add POST methods to APIView

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from . import serializers
 
 
 # Create your views here.
@@ -10,6 +12,8 @@ class HelloApiView(APIView):
     """
     Test API View
     """
+
+    serializers_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """
@@ -27,3 +31,19 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """
+        Create a Hello message with our name
+        :param request: Object
+        :return: JSON
+        """
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
